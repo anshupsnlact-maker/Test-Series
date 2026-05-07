b43b0aba4ba0f44e35e4785d6a64a4781f39755c
anshupsnlact-maker <anshupsnlact@gmail.com>
2026-05-07 15:59:03 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// Is file ko secure rakhein. Ye aapki "Chabi" (Keys) hai.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZMtyDal6GIvuwCWojzyWUe5DWiKfFGHc",
+  authDomain: "test-series-e94cf.firebaseapp.com",
+  projectId: "test-series-e94cf",
+  storageBucket: "test-series-e94cf.firebasestorage.app",
+  messagingSenderId: "1089405186531",
+  appId: "1:1089405186531:web:5c47280e47e5373bf4f0d6",
+  measurementId: "G-C25TJCVEJK"
+};
+
+// Aapka Admin Email yahan set karein
+export const ADMIN_EMAIL = "anshupsnlact@gmail.com"; 
